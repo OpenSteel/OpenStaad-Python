ee2ea196264f9308b246d536f3f35c38f4732dd7
OpenSteel <145235722+OpenSteel@users.noreply.github.com>
2023-09-16 19:04:22 -0500

    Update Use_Example.py

--- Use_Example.py
+++ Use_Example.py
@@ -1,12 +1,9 @@
-from ops_get import *
-
-# Create an instance of the class
-geometry = StaadProGeometry()
+import ops_get as ops
 
 # Get the number of nodes
-node_count = geometry.GetNodeCount()
+node_count = ops.GetNodeCount()
 print(f"There are {node_count} nodes in the model.")
 
 # Get the coordinates of the first node
-coordinates = geometry.GetNodeCoordinates(1)
+coordinates = ops.GetNodeCoordinates(1)
 print(f"The coordinates of the first node are ({coordinates[0]}, {coordinates[1]}, {coordinates[2]}).")
